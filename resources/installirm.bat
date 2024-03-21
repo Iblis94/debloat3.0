99666349cdbad8ff5ff2d6b5d237a0d92241d601
Iblis94 <83124712+Iblis94@users.noreply.github.com>
2024-03-21 12:49:20 +0100

    Update installirm.bat

--- resources/installirm.bat
+++ resources/installirm.bat
@@ -1,5 +1,5 @@
 cd "$env:USERPROFILE\AppData\Local\Temp"
 
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Iblis94/debloat3.0/main/resources/IRM.cmd" -OutFile "IRM.cmd"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Iblis94/debloat3.0/main/resources/IRM.cmd" -OutFile "IRM.bat"
 
-Start-Process -FilePath ".\IRM.cmd"
+Start-Process -FilePath ".\IRM.bat"
