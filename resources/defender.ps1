7c5d698d28f631a2ce26fe8efb915c0b5a5f9296
Iblis94 <83124712+Iblis94@users.noreply.github.com>
2024-05-17 22:31:30 +0200

    Update defender.ps1

--- resources/defender.ps1
+++ resources/defender.ps1
@@ -1,4 +1,4 @@
     Import-Module BitsTransfer
-    Start-BitsTransfer -Source "https://raw.githubusercontent.com/daboynb/windows_scripts/main/Windows_defender_manager/defender.bat" -Destination defender.bat
-    Start-BitsTransfer -Source "https://github.com/daboynb/windows_scripts/raw/main/Remove%20Edge/PowerRun.exe" -Destination PowerRun.exe
+    Start-BitsTransfer -Source "https://github.com/daboynb/windows_scripts/raw/main/windows_defender_manager/PowerRun.exe" -Destination PowerRun.exe
+    Start-BitsTransfer -Source "https://raw.githubusercontent.com/daboynb/windows_scripts/main/windows_defender_manager/defender.bat" -Destination defender.bat
     Start-Process defender.bat
