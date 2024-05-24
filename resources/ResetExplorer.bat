53dc9bf13360c3fa356516566b6fd7b49e61a237
Iblis94 <83124712+Iblis94@users.noreply.github.com>
2024-05-24 13:50:46 +0200

    Create ResetExplorer.bat

---
+++ resources/ResetExplorer.bat
@@ -0,0 +1,10 @@
+@echo off
+
+ie4uinit -show
+taskkill /IM explorer.exe /F
+timeout /t 2
+@reg delete "HKCU\Software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\TrayNotify" /v "PastIconsStream" /f
+@reg delete "HKCU\Software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\TrayNotify" /v "IconStreams" /f
+timeout /t 2
+start explorer.exe
+shutdown -r -f -t 5
