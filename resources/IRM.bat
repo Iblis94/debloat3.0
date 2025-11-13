f760313573e5716f2df118fc590bb2da6cdc64a6
Iblis94 <83124712+Iblis94@users.noreply.github.com>
2025-11-13 20:57:53 +0100

    Update IRM.bat

--- resources/IRM.bat
+++ resources/IRM.bat
@@ -72,26 +72,8 @@ start "" "https://t.me/WindowsItalyISO"
 exit
 
 :Script
-cls
-echo "Downloading"
-IF exist "C:\Users\%USERNAME%\Desktop" (
-  set "path_to_use=C:\Users\%USERNAME%\Desktop"
-)
-
-IF exist "C:\Users\%USERNAME%\OneDrive\Desktop" (
-  set "path_to_use=C:\Users\%USERNAME%\OneDrive\Desktop"
-)
-
-IF exist "%path_to_use%\windows_scripts-main" (
-  rmdir /s /q "%path_to_use%\windows_scripts-main"
-)
-
-powershell -command "$wc = New-Object net.webclient; $wc.DownloadFile('https://github.com/daboynb/windows_scripts/archive/refs/heads/main.zip', '%path_to_use%\main.zip'.Replace('path_to_use', '%path_to_use%'))"
-powershell -command "Expand-Archive -Path '%path_to_use%\main.zip' -DestinationPath '%path_to_use%' -Force"
-
-del /f "%path_to_use%\main.zip"
-
-echo "The downloaded folder is located on %path_to_use%"
+powershell -command "irm -Uri "https://raw.githubusercontent.com/daboynb/windows_scripts/refs/heads/main/utilities/utilities_gui.ps1" | iex"
 
 timeout 04
 exit
+
